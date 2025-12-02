9e0afa11c049cb3241850fbbc9475e97b9a2af3e
JYJ <2534803898@qq.com>
2025-12-02 14:02:26 +0800

    ..

--- Tcp/TcpServiceReadAsyncConsoleApp/Program.cs
+++ Tcp/TcpServiceReadAsyncConsoleApp/Program.cs
@@ -23,8 +23,13 @@ internal class Program
     {
         var service = new TcpService();
         await service.SetupAsync(new TouchSocketConfig()//载入配置
-                      //.SetListenIPHosts("tcp://127.0.0.1:7789", 7790)//同时监听两个地址
-                      .SetListenIPHosts("tcp://127.0.0.1:5019", 5020)//同时监听两个地址
+                                                        //.SetListenIPHosts("tcp://127.0.0.1:7789", 7790)//同时监听两个地址
+                                                        //.SetListenIPHosts("tcp://172.27.229.102:5019", 5012)//同时监听两个地址
+                                                        //.SetListenIPHosts("127.0.0.1:5012")//同时监听两个地址
+              .SetListenIPHosts(new IPHost("127.0.0.1:5019"))
+
+             //.SetListenIPHosts("tcp://8.130.37.131:5019", 5020)//同时监听两个地址
+             //.SetListenIPHosts("tcp://0.0.0.0:5019", 5020)//同时监听两个地址
 
              .ConfigureContainer(a =>//容器的配置顺序应该在最前面
              {
@@ -47,8 +52,94 @@ internal class Program
         await service.StartAsync();//启动
 
         Console.ReadKey();
+
+    }
+
+    /// <summary>
+    /// 以Received异步委托接收数据
+    /// </summary>
+    private static async Task RunClientForReceived()
+    {
+        var client = new TcpClient();
+        client.Connected = (client, e) => { return EasyTask.CompletedTask; };//成功连接到服务器
+        client.Closed = (client, e) => { return EasyTask.CompletedTask; };//从服务器断开连接，当连接不成功时不会触发。
+        client.Received = (client, e) =>
+        {
+            //从服务器收到信息
+            var mes = e.Memory.Span.ToString(Encoding.UTF8);
+            client.Logger.Info($"客户端接收到信息：{mes}");
+            return EasyTask.CompletedTask;
+        };
+
+        await client.SetupAsync(new TouchSocketConfig()
+                .SetRemoteIPHost(new IPHost("127.0.0.1:5012"))
+                .ConfigurePlugins(a =>
+                {
+                    a.UseReconnection<TcpClient>(options =>
+                    {
+                        options.PollingInterval = TimeSpan.FromSeconds(1);
+                    });
+                })
+                .ConfigureContainer(a =>
+                {
+                    a.AddConsoleLogger();//添加一个日志注入
+                }));//载入配置
+        await client.ConnectAsync();//连接
+        client.Logger.Info("客户端成功连接");
+
+        Console.WriteLine("输入任意内容，回车发送");
+        while (true)
+        {
+            await client.SendAsync(Console.ReadLine());
+        }
+    }
+
+    private static async Task RunClientForReadAsync()
+    {
+        #region Tcp客户端异步阻塞接收
+        var client = new TcpClient();
+        await client.ConnectAsync("tcp://127.0.0.1:7789");//连接
+
+        client.Logger.Info("客户端成功连接");
+
+        Console.WriteLine("输入任意内容，回车发送");
+        //receiver可以复用，不需要每次接收都新建
+        using (var receiver = client.CreateReceiver())
+        {
+            while (true)
+            {
+                //发送信息
+                await client.SendAsync(Console.ReadLine());
+
+                //设置接收超时
+                using (var cts = new CancellationTokenSource(1000 * 60))
+                {
+                    //receiverResult必须释放
+                    using (var receiverResult = await receiver.ReadAsync(cts.Token))
+                    {
+                        if (receiverResult.IsCompleted)
+                        {
+                            //断开连接了
+                        }
+
+                        //从服务器收到信息。
+                        var mes = receiverResult.Memory.Span.ToString(Encoding.UTF8);
+                        client.Logger.Info($"客户端接收到信息：{mes}");
+
+                        //如果是适配器信息，则可以直接获取receiverResult.RequestInfo;
+                    }
+                }
+
+            }
+        }
+        #endregion
+
     }
 }
+
+
+  
+
 
 internal class TcpServiceReceiveAsyncPlugin : PluginBase, ITcpConnectedPlugin
 {
